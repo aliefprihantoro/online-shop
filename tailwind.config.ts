8ef2d38e1e1a92de41a1797169609a6747b6a847
aliefprihantoro <aliefprihantoro@gmail.com>
2024-12-02 11:22:38 +0700

    chore: setup tailwindcss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,32 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
+import daisyui from 'daisyui'
+import themes from 'daisyui/src/theming/themes'
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        Lato: ['Lato', 'sans-serif'],
+        Rokkit: ['Rokkitt', 'serif'],
+      },
+      boxShadow: {
+        center: '0 0 15px 3px blur',
       },
     },
   },
-  plugins: [],
-} satisfies Config;
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...themes['emerald'],
+        },
+      },
+    ],
+  },
+  plugins: [daisyui],
+} satisfies Config
